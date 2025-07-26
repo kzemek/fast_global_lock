31117461fa25423192f733789ac5e34093aeef53
Konrad Zemek <konrad.zemek@gmail.com>
2025-07-26 21:57:11 +0200

    Add ergonomic overloads to the API

--- lib/fast_global_lock.ex
+++ lib/fast_global_lock.ex
@@ -2,10 +2,18 @@ defmodule FastGlobalLock do
   alias FastGlobalLock.Internal.LockHolder
   alias FastGlobalLock.Internal.Utils
 
-  @spec set_lock(key :: term(), [node()] | nil, timeout()) :: boolean()
-  def set_lock(key, nodes \\ nil, timeout \\ :infinity) do
-    nodes = nodes || Node.list([:this, :visible])
+  @spec set_lock(key :: term()) :: boolean()
+  def set_lock(key), do: set_lock(key, nodes(), :infinity)
 
+  @spec set_lock(key :: term(), [node()] | timeout()) :: boolean()
+  def set_lock(key, node_or_timeout)
+  def set_lock(key, [node | _] = nodes) when is_atom(node), do: set_lock(key, nodes, :infinity)
+  def set_lock(key, timeout) when is_integer(timeout), do: set_lock(key, nodes(), timeout)
+  def set_lock(key, :infinity), do: set_lock(key, nodes(), :infinity)
+
+  @spec set_lock(key :: term(), [node()], timeout()) :: boolean()
+  def set_lock(key, [node | _] = nodes, timeout)
+      when is_atom(node) and (is_integer(timeout) or timeout == :infinity) do
     case Utils.whereis_lock(key, nodes) do
       {on_behalf_of, {owner_pid, _lock_ref}} when on_behalf_of == self() ->
         GenServer.cast(owner_pid, :nest_lock)
@@ -17,10 +25,11 @@ defmodule FastGlobalLock do
     end
   end
 
-  @spec del_lock(key :: term(), [node()] | nil) :: boolean()
-  def del_lock(key, nodes \\ nil) do
-    nodes = nodes || Node.list([:this, :visible])
+  @spec del_lock(key :: term()) :: boolean()
+  def del_lock(key), do: del_lock(key, nodes())
 
+  @spec del_lock(key :: term(), [node()]) :: boolean()
+  def del_lock(key, [node | _] = nodes) when is_atom(node) do
     case Utils.whereis_lock(key, nodes) do
       {on_behalf_of, {owner_pid, _lock_ref}} when on_behalf_of == self() ->
         GenServer.call(owner_pid, :del_lock, :infinity)
@@ -30,10 +39,18 @@ defmodule FastGlobalLock do
     end
   end
 
-  @spec trans(key :: term(), fun :: (-> any()), [node()] | nil, timeout()) :: any() | :aborted
-  def trans(key, fun, nodes \\ nil, timeout \\ :infinity) do
-    nodes = nodes || Node.list([:this, :visible])
+  @spec trans(key :: term(), (-> any())) :: any() | :aborted
+  def trans(key, fun), do: trans(key, fun, nodes(), :infinity)
 
+  @spec trans(key :: term(), (-> any()), [node()] | timeout()) :: boolean()
+  def trans(key, fun, node_or_timeout)
+  def trans(key, fun, [nod | _] = nodes) when is_atom(nod), do: trans(key, fun, nodes, :infinity)
+  def trans(key, fun, timeout) when is_integer(timeout), do: trans(key, fun, nodes(), timeout)
+  def trans(key, fun, :infinity), do: trans(key, fun, nodes(), :infinity)
+
+  @spec trans(key :: term(), (-> any()), [node()], timeout()) :: any() | :aborted
+  def trans(key, fun, [node | _] = nodes, timeout)
+      when is_atom(node) and (is_integer(timeout) or timeout == :infinity) do
     if set_lock(key, nodes, timeout) do
       try do
         fun.()
@@ -44,4 +61,6 @@ defmodule FastGlobalLock do
       :aborted
     end
   end
+
+  defp nodes, do: Node.list([:this, :visible])
 end
