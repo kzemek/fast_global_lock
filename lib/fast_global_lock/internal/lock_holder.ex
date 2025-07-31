8797a7cf16f5faba07ea35434bdb9d15fdf9ab53
Konrad Zemek <konrad.zemek@gmail.com>
2025-07-31 10:17:30 +0200

    Ensure notification mechanism functions with multiple peers
    The code only notified one peer, and then the peer list died together
    with the previous lock holder. Other peers would not be notified, and
    would only notice on the next timeout message. Thus, this stopped being
    a "fast" lock on contention.
    
    This commit fixes the issue by:
    - monitoring lock holders and acting on their down message
      - possibly to lock, but more likely to refresh the current owner
    - notifying the first waiting peer synchronously to give it a head
      start, and to give it a sorted list of waiting peers

--- lib/fast_global_lock/internal/lock_holder.ex
+++ lib/fast_global_lock/internal/lock_holder.ex
@@ -2,6 +2,8 @@ defmodule FastGlobalLock.Internal.LockHolder do
   @moduledoc false
   use GenServer
 
+  require Logger
+
   alias __MODULE__, as: State
   alias FastGlobalLock.Internal.Utils, as: Utils
 
@@ -14,7 +16,8 @@ defmodule FastGlobalLock.Internal.LockHolder do
     lock_count: 0,
     times: 0,
     peers: %{},
-    peer_generation: 0
+    peer_generation: 0,
+    had_lock?: false
   ]
 
   @typep state :: %State{
@@ -24,98 +27,164 @@ defmodule FastGlobalLock.Internal.LockHolder do
            notified_owner: Utils.global_owner() | nil,
            lock_count: non_neg_integer(),
            times: non_neg_integer(),
-           peers: [pid()],
-           peer_generation: non_neg_integer()
+           peers: %{optional(pid()) => non_neg_integer()},
+           peer_generation: non_neg_integer(),
+           had_lock?: boolean()
          }
+
+  defguardp has_lock(state) when state.lock_count > 0
 
   @impl GenServer
   def init({resource, nodes}),
     do: {:ok, %State{resource: resource, nodes: nodes}}
 
   @impl GenServer
-  def handle_call({:set_lock, timeout}, parent, %State{lock_count: 0} = state) do
+  def handle_call({:set_lock, _timeout}, _parent, %State{} = state) when has_lock(state) do
+    Logger.warning("FastGlobalLock: set_lock called on locked state")
+    {:reply, true, %{state | lock_count: state.lock_count + 1}}
+  end
+
+  def handle_call({:set_lock, timeout}, parent, %State{} = state) do
     if timeout != :infinity, do: :erlang.send_after(timeout, self(), :lock_timeout)
-    try_lock(%{state | parent: parent})
+    noreply_try_lock(%{state | parent: parent})
   end
 
-  def handle_call(:del_lock, from, %State{lock_count: 1} = state) do
-    del_lock(state)
-    GenServer.reply(from, true)
-    {:stop, :normal, %{state | lock_count: 0}}
+  def handle_call(:del_lock, _from, %State{lock_count: 1} = state) do
+    new_state = del_global_lock(state)
+    {:reply, true, new_state, {:continue, :stop}}
   end
 
-  def handle_call(:del_lock, _from, %State{lock_count: lock_count} = state) when lock_count > 0 do
-    {:reply, true, %{state | lock_count: lock_count - 1}}
+  def handle_call(:del_lock, _from, %State{} = state) when has_lock(state),
+    do: {:reply, true, %{state | lock_count: state.lock_count - 1}}
+
+  def handle_call(:del_lock, _from, %State{} = state) do
+    Logger.error("FastGlobalLock: del_lock called on unlocked state")
+    {:stop, :del_lock_on_unlocked_state, state}
+  end
+
+  def handle_call({:peer_released_lock, peers}, _from, %State{} = state) do
+    new_state = try_lock(state)
+
+    next_step =
+      if has_lock(new_state),
+        do: {:continue, {:take_over_peers, peers}},
+        else: random_sleep(new_state.times)
+
+    {:reply, :ok, new_state, next_step}
   end
 
   @impl GenServer
-  def handle_cast(:nest_lock, %State{} = state),
-    do: {:noreply, %{state | lock_count: state.lock_count + 1}}
+  def handle_continue({:take_over_peers, peers}, %State{} = state) do
+    # There's an edge case where this process aquired the lock before receiving
+    # `:peer_released_lock` message. In this case, we can't tell the ordering between the current
+    # peers and the received peers, so we just merge them and allow duplicate generations
+    peers = Map.merge(peers, state.peers)
 
-  def handle_cast({:awaiting_lock, peer}, %State{} = state) when is_map_key(state.peers, peer) do
-    {:noreply, state}
+    peer_generation = Enum.max([-1 | Map.values(peers)]) + 1
+    Enum.each(Map.keys(peers), &Process.monitor/1)
+    {:noreply, %{state | peers: peers, peer_generation: peer_generation}}
   end
 
-  def handle_cast({:awaiting_lock, peer}, %State{} = state) do
+  def handle_continue(:stop, %State{} = state),
+    do: {:stop, :normal, state}
+
+  @impl GenServer
+  def handle_cast(:nest_lock, %State{} = state) when has_lock(state),
+    do: {:noreply, %{state | lock_count: state.lock_count + 1}}
+
+  def handle_cast(:nest_lock, %State{} = state) do
+    Logger.error("FastGlobalLock: nest_lock called on unlocked state")
+    {:stop, :nest_lock_on_unlocked_state, state}
+  end
+
+  def handle_cast({:peer_awaiting_lock, peer}, %State{} = state)
+      when is_map_key(state.peers, peer),
+      do: {:noreply, state}
+
+  def handle_cast({:peer_awaiting_lock, peer}, %State{} = state) do
     Process.monitor(peer)
     peer_num = state.peer_generation
     peers = Map.put(state.peers, peer, peer_num)
     {:noreply, %{state | peers: peers, peer_generation: peer_num + 1}}
   end
 
-  def handle_cast(:released_lock, %State{} = state),
-    do: try_lock(state)
-
   @impl GenServer
-  def handle_info(:lock_timeout, %State{lock_count: 0} = state) do
+  def handle_info(:lock_timeout, %State{} = state) when has_lock(state),
+    do: {:noreply, state}
+
+  def handle_info(:lock_timeout, %State{} = state) do
     GenServer.reply(state.parent, false)
     {:stop, :normal, state}
   end
 
-  def handle_info(:lock_timeout, %State{} = state),
-    do: {:noreply, state}
-
   def handle_info(:timeout, %State{} = state),
-    do: try_lock(state)
+    do: noreply_try_lock(state)
 
-  def handle_info({:DOWN, _ref, :process, pid, _reason}, %State{} = state),
+  def handle_info({:DOWN, _ref, :process, pid, _reason}, %State{} = state) when has_lock(state),
     do: {:noreply, %{state | peers: Map.delete(state.peers, pid)}}
 
-  @impl GenServer
-  def terminate(_reason, %State{} = state),
-    do: del_lock(state)
+  def handle_info({:DOWN, _ref, :process, _pid, _reason}, %State{} = state),
+    do: noreply_try_lock(state)
 
-  @spec try_lock(state()) :: {:noreply, state()} | {:noreply, state(), timeout()}
-  defp try_lock(%State{lock_count: lock_count} = state) when lock_count > 0,
+  def handle_info({ref, _late_genserver_reply}, %State{} = state) when is_reference(ref),
     do: {:noreply, state}
+
+  @impl GenServer
+  def terminate(_reason, %State{} = state) do
+    if has_lock(state), do: del_global_lock(state)
+    if state.had_lock?, do: release_lock_to_first_peer(state.peers)
+    :ok
+  end
+
+  defp noreply_try_lock(state) do
+    new_state = try_lock(state)
+
+    if has_lock(new_state),
+      do: {:noreply, new_state},
+      else: {:noreply, new_state, random_sleep(new_state.times)}
+  end
+
+  @spec try_lock(state()) :: state()
+  defp try_lock(%State{} = state) when has_lock(state),
+    do: state
 
   defp try_lock(%State{parent: {parent_pid, _tag}} = state) do
     if :global.set_lock({state.resource, parent_pid}, state.nodes, 0) do
-      GenServer.reply(state.parent, true)
       Process.flag(:trap_exit, true)
-      {:noreply, %{state | lock_count: state.lock_count + 1}}
+      GenServer.reply(state.parent, true)
+      %{state | lock_count: state.lock_count + 1, had_lock?: true}
     else
       notified_owner = notify_lock_owner(state.resource, state.nodes, state.notified_owner)
       new_state = %{state | notified_owner: notified_owner, times: state.times + 1}
 
-      if is_nil(notified_owner),
-        do: try_lock(new_state),
-        else: {:noreply, new_state, random_sleep(state.times)}
+      if is_nil(notified_owner) do
+        try_lock(new_state)
+      else
+        if notified_owner != state.notified_owner, do: monitor_owner(notified_owner)
+        new_state
+      end
     end
   end
 
-  @spec del_lock(state()) :: :ok
-  defp del_lock(%State{lock_count: 0}),
+  defp monitor_owner({pid, _lock_ref}),
+    do: Process.monitor(pid)
+
+  @spec del_global_lock(state()) :: state()
+  defp del_global_lock(%State{parent: {parent_pid, _tag}} = state) when has_lock(state) do
+    :global.del_lock({state.resource, parent_pid}, state.nodes)
+    %{state | lock_count: 0}
+  end
+
+  defp release_lock_to_first_peer(peers) when is_map(peers),
+    do: peers |> Enum.sort_by(fn {_peer, num} -> num end) |> release_lock_to_first_peer()
+
+  defp release_lock_to_first_peer([]),
     do: :ok
 
-  defp del_lock(%State{parent: {parent_pid, _tag}} = state) do
-    :global.del_lock({state.resource, parent_pid}, state.nodes)
-    Process.flag(:trap_exit, false)
-
-    {peer, _} = Enum.min_by(state.peers, fn {_peer, peer_num} -> peer_num end, fn -> {nil, 0} end)
-    if peer, do: GenServer.cast(peer, :released_lock)
-
-    :ok
+  defp release_lock_to_first_peer([{first_peer, _peer_num} | peers]) do
+    GenServer.call(first_peer, {:peer_released_lock, Map.new(peers)}, 100)
+  catch
+    :exit, _ -> release_lock_to_first_peer(peers)
   end
 
   @spec notify_lock_owner(term(), [node()], Utils.global_owner() | nil) ::
@@ -129,7 +198,7 @@ defmodule FastGlobalLock.Internal.LockHolder do
   end
 
   defp do_notify_lock_owner(resource, nodes, {owner_pid, _lock_ref} = owner) do
-    GenServer.cast(owner_pid, {:awaiting_lock, self()})
+    GenServer.cast(owner_pid, {:peer_awaiting_lock, self()})
 
     # the owner may have changed from under us while we were notifying it
     case Utils.whereis_lock(resource, nodes) do
